45e7934932def1920557e296f02e1a78666e530f
shuvendu <shuvendusingha17@gmail.com>
2024-12-12 23:33:45 +0530

    db connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,2 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected`);
-    } catch (error) {
-        console.error(`error: ${error.message}`)
-        process.exit(1) //1 = error, 0 = success
-    }
-}
+export const MONGO_URI = 'mongodb+srv://admin:admin@testcluster.p4mop.mongodb.net/'
+export const PORT = 5000
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,80 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import { connectDB } from './config/db.js';
+import express, { request, response } from "express";
+import { PORT, MONGO_URI } from "./config/db.js";
+import mongoose from "mongoose";
+import cors from "cors";
+import { Product } from "./models/product.js"
 
-dotenv.config();
 const app = express();
 
-app.post("/products", (req, res) => {
-    res.send('Server is ready')
-})
+// middleware for parsing request body
+app.use(express.json());
 
-app.listen(5000, () => {
-    connectDB();
-    console.log("Server started at port 5000...")
+// middleware for handeling CORS
+
+//option 1
+app.use(cors());
+
+//option 2
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowHeaders: ['Content-Type'],
+// })
+// )
+
+app.get("/api/products", async(request, response) => {
+    try {
+        const products = await Product.find({})
+        return response.status(200).json({success: true, count: products.length, data: products});
+    } catch (error) {
+        console.log('error in fetching product:', error.message)
+        response.status(500).json({success: false, message: 'server error'})
+    }
 });
+
+app.post("/api/products", async (request, response) => {
+    const product = request.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return response
+            .status(400)
+            .send({ success: false, message: "Provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        response.status(201).json({
+            success: true,
+            message: "product created successfullly",
+            data: newProduct,
+        });
+    } catch (error) {
+        console.error("error creating product", error.message);
+        response.status(500).json({ success: false, message: "server error" });
+    }
+});
+
+app.delete("/api/delete/:id", async(request, response) => {
+    const {id} = request.params;
+
+    try {
+        await Product.findByIdAndDelete(id)
+        response.status(200).json({success: true, message: "Product Deleted"})
+    } catch (error) {
+        response.status(500).json({success: false, message: "Product not found"})
+    }
+}) 
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("app connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is running on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
